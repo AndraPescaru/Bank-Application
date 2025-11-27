c0d59513b42747a834170811c98ecc961124dfce
AndraPescaru <andrapescaru21@yahoo.com>
2025-11-27 08:27:39 +0200

    task 2 ex 5

--- src/com/luxoft/bankapp/domain/Bank.java
+++ src/com/luxoft/bankapp/domain/Bank.java
@@ -1,85 +1,96 @@
 package com.luxoft.bankapp.domain;
 
 import java.text.DateFormat;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Set;
+import java.util.HashSet;
 
 import com.luxoft.bankapp.exceptions.ClientExistsException;
+import com.luxoft.bankapp.service.EmailService;
 import com.luxoft.bankapp.utils.ClientRegistrationListener;
 
 public class Bank {
 
-	private final Set<Client> clients = new HashSet<Client>();
-	private final List<ClientRegistrationListener> listeners = new ArrayList<ClientRegistrationListener>();
+    private final Set<Client> clients = new HashSet<>();
+    private final List<ClientRegistrationListener> listeners = new ArrayList<>();
+    private final EmailService emailService = new EmailService();
 
-	private int printedClients = 0;
-	private int emailedClients = 0;
-	private int debuggedClients = 0;
+    private int printedClients = 0;
+    private int emailedClients = 0;
+    private int debuggedClients = 0;
 
-	public Bank() {
-		listeners.add(new PrintClientListener());
-		listeners.add(new EmailNotificationListener());
-		listeners.add(new DebugListener());
-	}
+    public Bank() {
+        listeners.add(new PrintClientListener());
+        listeners.add(new EmailNotificationListener());
+        listeners.add(new DebugListener());
+    }
 
-	public int getPrintedClients() {
-		return printedClients;
-	}
+    public int getPrintedClients() {
+        return printedClients;
+    }
 
-	public int getEmailedClients() {
-		return emailedClients;
-	}
+    public int getEmailedClients() {
+        return emailedClients;
+    }
 
-	public int getDebuggedClients() {
-		return debuggedClients;
-	}
+    public int getDebuggedClients() {
+        return debuggedClients;
+    }
 
-	public void addClient(final Client client) throws ClientExistsException {
-		boolean added = clients.add(client);
-		if (!added) {
-			throw new ClientExistsException("Client already exists into the bank");
-		}
-		notify(client);
-	}
+    public void addClient(final Client client) throws ClientExistsException {
+        boolean added = clients.add(client);
+        if (!added) {
+            throw new ClientExistsException("Client already exists into the bank");
+        }
+        notify(client);
+    }
 
-	private void notify(Client client) {
-		for (ClientRegistrationListener listener : listeners) {
-			listener.onClientAdded(client);
-		}
-	}
+    private void notify(Client client) {
+        for (ClientRegistrationListener listener : listeners) {
+            listener.onClientAdded(client);
+        }
+    }
 
-	public Set<Client> getClients() {
-		return Collections.unmodifiableSet(clients);
-	}
+    public Set<Client> getClients() {
+        return Collections.unmodifiableSet(clients);
+    }
 
-	class PrintClientListener implements ClientRegistrationListener {
-		@Override
-		public void onClientAdded(Client client) {
-			System.out.println("Client added: " + client.getName());
-			printedClients++;
-		}
+    public void close() {
+        emailService.close();
+    }
 
-	}
+    class PrintClientListener implements ClientRegistrationListener {
+        @Override
+        public void onClientAdded(Client client) {
+            System.out.println("Client added: " + client.getName());
+            printedClients++;
+        }
+    }
 
-	class EmailNotificationListener implements ClientRegistrationListener {
-		@Override
-		public void onClientAdded(Client client) {
-			System.out.println("Notification email for client " + client.getName() + " to be sent");
-			emailedClients++;
-		}
-	}
+    class EmailNotificationListener implements ClientRegistrationListener {
+        @Override
+        public void onClientAdded(Client client) {
+            Email email = new Email(
+                    client,
+                    "bank@bank.com",
+                    client.getName().replace(" ", "").toLowerCase() + "@mail.com",
+                    "Welcome",
+                    "Hello " + client.getName()
+            );
+            emailService.sendNotificationEmail(email);
+            emailedClients++;
+        }
+    }
 
-	class DebugListener implements ClientRegistrationListener {
-		@Override
-		public void onClientAdded(Client client) {
-			System.out.println("Client " + client.getName() + " added on: " +
-					DateFormat.getDateInstance(DateFormat.FULL).format(new Date()));
-			debuggedClients++;
-		}
-	}
-
+    class DebugListener implements ClientRegistrationListener {
+        @Override
+        public void onClientAdded(Client client) {
+            System.out.println("Client " + client.getName() + " added on: "
+                    + DateFormat.getDateInstance(DateFormat.FULL).format(new Date()));
+            debuggedClients++;
+        }
+    }
 }
--- src/com/luxoft/bankapp/domain/Client.java
+++ src/com/luxoft/bankapp/domain/Client.java
@@ -1,69 +1,68 @@
 package com.luxoft.bankapp.domain;
 
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Set;
+import java.util.HashSet;
 
 public class Client {
 
-	private String name;
-	private Gender gender;
+    private String name;
+    private Gender gender;
+    private String city;
+    private final Set<Account> accounts = new HashSet<>();
 
-	private Set<Account> accounts = new HashSet<Account>();
+    public Client(String name, Gender gender) {
+        this.name = name;
+        this.gender = gender;
+    }
 
-	private String city;
+    public void addAccount(final Account account) {
+        accounts.add(account);
+    }
 
-	public Client(String name, Gender gender, String city) {
-		this.name = name;
-		this.gender = gender;
-		this.city = city;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getCity() {
-		return city;
-	}
+    public Gender getGender() {
+        return gender;
+    }
 
-	public void addAccount(final Account account) {
-		accounts.add(account);
-	}
+    public String getCity() {
+        return city;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setCity(String city) {
+        this.city = city;
+    }
 
-	public Gender getGender() {
-		return gender;
-	}
+    public Set<Account> getAccounts() {
+        return Collections.unmodifiableSet(accounts);
+    }
 
-	public Set<Account> getAccounts() {
-		return Collections.unmodifiableSet(accounts);
-	}
+    public String getClientGreeting() {
+        if (gender != null) {
+            return gender.getGreeting() + " " + name;
+        } else {
+            return name;
+        }
+    }
 
-	public String getClientGreeting() {
-		if (gender != null) {
-			return gender.getGreeting() + " " + name;
-		} else {
-			return name;
-		}
-	}
+    @Override
+    public String toString() {
+        return getClientGreeting();
+    }
 
-	@Override
-	public String toString() {
-		return getClientGreeting();
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return name != null && name.equals(client.name);
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-
-		Client client = (Client) o;
-
-		return name != null ? name.equals(client.name) : client.name == null;
-	}
-
-	@Override
-	public int hashCode() {
-		return name != null ? name.hashCode() : 0;
-	}
+    @Override
+    public int hashCode() {
+        return name == null ? 0 : name.hashCode();
+    }
 }
